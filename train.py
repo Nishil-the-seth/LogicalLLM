0745cc0607028f83bc7d5b50ccfd09255d4df837
Nishil Seth Gupta <nishilsethgupta@gmail.com>
2025-06-28 03:55:29 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,26 +1,28 @@
-from dataset import load_data
 from mlp import MLP
-import matplotlib.pyplot as plt
+from dataset import load_data
+from plot import plot_loss
+import random
 
-# Load and split data
 X_train, y_train, X_test, y_test = load_data()
-
-# Create model
 model = MLP(input_size=2, hidden_size=4, output_size=1)
 
-# Train
 epochs = 1000
 batch_size = 16
+learning_rate = 0.1
 losses = []
 
 for epoch in range(epochs):
-    # Manually split into mini-batches and call model.train_batch(...)
-    # Append loss to losses[]
-    pass
+    combined = list(zip(X_train, y_train))
+    random.shuffle(combined)
+    X_train[:], y_train[:] = zip(*combined)
 
-# Plot loss
-plt.plot(losses)
-plt.title("Loss Curve")
-plt.xlabel("Epoch")
-plt.ylabel("Loss")
-plt.show()
+    total_loss = 0
+    for i in range(0, len(X_train), batch_size):
+        batch_x = X_train[i:i+batch_size]
+        batch_y = y_train[i:i+batch_size]
+        loss = model.train_batch(batch_x, batch_y, learning_rate)
+        total_loss += loss
+    avg_loss = total_loss / (len(X_train) // batch_size)
+    losses.append(avg_loss)
+
+plot_loss(losses)
